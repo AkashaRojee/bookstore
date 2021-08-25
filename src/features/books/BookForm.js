dfe9290a35171b109bb246e5453729c09a873e12
Akasha <Akasha.Rojee@gmail.com>
2021-08-26 02:26:06 +0400

    Add title to form

--- src/features/books/BookForm.js
+++ src/features/books/BookForm.js
@@ -2,16 +2,19 @@ import React from 'react';
 import styles from './BookForm.module.scss';
 
 const BookForm = () => (
-  <form className={styles.form}>
-    <input type="text" placeholder="Book title" />
-    <select>
-      <option value="" disabled>Category</option>
-      <option value="Action">Action</option>
-      <option value="Science Fiction">Science Fiction</option>
-      <option value="Economy">Economy</option>
-    </select>
-    <button type="submit">ADD BOOK</button>
-  </form>
+  <div>
+    <h2>ADD NEW BOOK</h2>
+    <form className={styles.form}>
+      <input type="text" placeholder="Book title" />
+      <select>
+        <option value="" disabled>Category</option>
+        <option value="Action">Action</option>
+        <option value="Science Fiction">Science Fiction</option>
+        <option value="Economy">Economy</option>
+      </select>
+      <button type="submit">ADD BOOK</button>
+    </form>
+  </div>
 );
 
 export default BookForm;
