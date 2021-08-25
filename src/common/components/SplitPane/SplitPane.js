2127eb2c53c6cef18ae2efe37b1adf9c20fb97ff
Akasha <Akasha.Rojee@gmail.com>
2021-08-25 20:46:02 +0400

    Structure SplitPane

--- src/common/components/SplitPane/SplitPane.js
+++ src/common/components/SplitPane/SplitPane.js
@@ -2,10 +2,10 @@ import styles from './SplitPane.module.scss';
 
 const SplitPane = ({ layout, first, second }) => (
   <div className={`${styles.SplitPane} ${styles[layout.pane]}`}>
-    <div className={styles[layout.first]}>
+    <div className={`${styles.first} ${styles[layout.first]}`}>
       {first}
     </div>
-    <div className={styles[layout.second]}>
+    <div className={`${styles.second} ${styles[layout.second]}`}>
       {second}
     </div>
   </div>
