f7a8657d5e7187a3bb731e52b781a194569f9580
Akasha <Akasha.Rojee@gmail.com>
2021-08-26 04:09:09 +0400

    Update build

--- src/common/components/ProgressCircle/ProgressCircle.js
+++ src/common/components/ProgressCircle/ProgressCircle.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import styles from './ProgressCircle.module.css';
+import styles from './ProgressCircle.module.scss';
 
 const ProgressCircle = ({ value }) => (
   <div className={styles.circle} style={{ '--p': value }} />
