cc3c92afe9d937530fc390b06240c740fc1de6dc
Akasha <Akasha.Rojee@gmail.com>
2021-08-26 16:14:27 +0400

    Set form inputs to be controlled components

--- src/features/books/components/BookForm.js
+++ src/features/books/components/BookForm.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './BookForm.module.scss';
 
-const BookForm = () => (
-  <div className={styles['book-form']}>
-    <h2>ADD NEW BOOK</h2>
-    <form>
-      <input type="text" placeholder="Book title" />
-      <select>
-        <option value="" disabled>Category</option>
-        <option value="Action">Action</option>
-        <option value="Science Fiction">Science Fiction</option>
-        <option value="Economy">Economy</option>
-      </select>
-      <button type="submit">ADD BOOK</button>
-    </form>
-  </div>
-);
+const BookForm = () => {
+  const [input, setInput] = useState({
+    title: '',
+    author: '',
+  });
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setInput((prevInput) => ({
+      ...prevInput,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div className={styles['book-form']}>
+      <h2>ADD NEW BOOK</h2>
+      <form>
+        <input name="title" type="text" placeholder="Book title" value={input.title} onChange={(e) => handleInput(e)} />
+        <input name="author" type="text" placeholder="Author" value={input.author} onChange={(e) => handleInput(e)} />
+        <select>
+          <option value="" disabled>Category</option>
+          <option value="Action">Action</option>
+          <option value="Science Fiction">Science Fiction</option>
+          <option value="Economy">Economy</option>
+        </select>
+        <button type="submit">ADD BOOK</button>
+      </form>
+    </div>
+  );
+};
 
 export default BookForm;
