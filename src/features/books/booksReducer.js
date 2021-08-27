da93da8cd5aae5182909da6a781e8cf5a33f735f
Akasha <Akasha.Rojee@gmail.com>
2021-08-27 23:16:01 +0400

    Post book to API when clicking on Add Book button

--- src/features/books/booksReducer.js
+++ src/features/books/booksReducer.js
@@ -4,6 +4,32 @@ const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
 const initialState = [];
 
+const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/LDqE4jqcttj5TFLLJ2je/books';
+
+async function post(payload) {
+  await fetch(
+
+    url,
+
+    {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    },
+  );
+
+  
+}
+
+export const postBook = (payload) => {
+  return async function(dispatch) {
+    await post(payload); 
+    dispatch(addBook(payload));
+  };
+}
+
 export const addBook = (payload) => ({
   type: ADD_BOOK,
   payload,
--- src/features/books/logic/BookInfo.js
+++ src/features/books/logic/BookInfo.js
@@ -1,6 +1,6 @@
 export default class BookInfo {
   constructor(category, title, author) {
-    this.id = `${title}-${author}`;
+    this.item_id = `${title}-${author}`;
     this.category = category;
     this.title = title;
     this.author = author;
